e96b30f9f848f0f6c80b6ef3be7eadc2a3695f76
Jiabei L <luojiabei@gmail.com>
2016-06-08 23:16:14 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+var NUM_CIRCLESROW = 12;
+var circleDiameter;
+var circleRadius;
+
+function setup() {
+    createCanvas(480, 600);
+    circleDiameter = width/NUM_CIRCLESROW;
+    circleRadius = circleDiameter/2;
+}
+
+function draw() {
+    var isShifted = false;
+    
+    var y = height;
+    while (y >= 0) {
+       
+       var x;
+       
+           if(isShifted) {
+               x = circleRadius;
+           } 
+           else {
+               x = 0;
+           }
+       
+       while (x <= width) {
+          ellipse(x, y, circleDiameter, circleDiameter);
+          x = x + circleDiameter;
+       } 
+            
+        y = y - circleRadius;
+        isShifted = !isShifted;
+    }
+    
+}
